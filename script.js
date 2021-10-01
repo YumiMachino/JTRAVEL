1e739a916f336d1713396f8e265e90b749994c8f
YumiMachino <y.y.1515m@gmail.com>
2021-09-30 19:28:34 -0700

    Add CSS style

---
+++ script.js
@@ -0,0 +1,10 @@
+const section_titles = document.querySelectorAll(".section-title");
+
+console.log(section_titles)
+console.log(window.innerHeight)
+
+// window.addEventListener("scroll", slideIn);
+
+// function slideIn() {
+//   console.log(window.innerHeight);
+// }
